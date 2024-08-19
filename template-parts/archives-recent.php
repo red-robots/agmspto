af2e54f6603c651cfcb88f00c74abc0cd84f8246
accrane <austincrane@gmail.com>
2024-08-19 12:20:08 -0400

    bring back cal

--- template-parts/archives-recent.php
+++ template-parts/archives-recent.php
@@ -12,7 +12,7 @@
 			'post_type'=>'post',
 			'posts_per_page'=>10,
 			'orderby'=>'date',
-			'order'=>'ASC',
+			'order'=>'DESC',
 		);
 		$query = new WP_Query($args);
 		if($query->have_posts()):?>
